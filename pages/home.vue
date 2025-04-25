6d58d514313c271ddbf8a1b3bd69f839da728964
Beadd <flyships@qq.com>
2025-04-25 21:52:24 +0800

    refine page

--- pages/home.vue
+++ pages/home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="flex h-dvh w-full items-center justify-center">
-    <button class="btn" @click="useRouter().push('login')">Login</button>
+    <button class="btn" @click="useRouter().push('login')">
+      Login
+    </button>
   </div>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="flex h-dvh w-full items-center justify-center">
-    <button class="btn" @click="useRouter().push('home')">Home</button>
+    <button class="btn" @click="useRouter().push('home')">
+      Home
+    </button>
   </div>
 </template>
