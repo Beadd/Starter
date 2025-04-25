7bf8e22fd05a22309f5976cb74cf4a6da50e5857
Beadd <flyships@qq.com>
2025-04-25 19:44:35 +0800

    add the npm run

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@ const { locales, setLocale } = useI18n();
 </script>
 
 <template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
   <div>
     <button v-for="(locale, index) in locales" :key="index" @click="setLocale(locale.code)">
       {{ locale.name }}
